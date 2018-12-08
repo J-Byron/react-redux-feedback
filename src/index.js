113c46e3d0e10f21f65625612fc2085c04133926
Admin <admin@Admins-MacBook-Pro.local>
2018-12-08 14:03:12 -0600

    Adds redux to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
+
+// *----------* create-react-app *----------*
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// *----------* Redux *----------*
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+const reviewReducer = (state = {}, action) =>{
+    return state;
+}
+
+const storeInstance = createStore(
+    // This is a reducer. It's called when the page loads or
+    // dispatch is called.
+    combineReducers({
+        reviewReducer
+    }),
+    applyMiddleware(logger)
+);
+
+ReactDOM.render( <Provider store={storeInstance}><App/></Provider>, document.getElementById('root'));
 registerServiceWorker();
