6102e23d33ddf2eacbc722e05353b8095cf5aa1e
Admin <admin@Admins-MBP.PK5001Z>
2018-12-09 14:16:03 -0600

    adds styling for home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,20 +1,54 @@
+// *----------*  *----------*
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
+// *----------*  *----------*
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+
+const buttonTheme = createMuiTheme({ palette: 
+    { primary: {main: '#39A094'} } 
+})
+
+const styles = {
+    form: {
+        width: '80%',
+        height: 400,
+        margin: '40px 10%',
+        backgroundColor: '#eee',
+        borderRadius: '6px',
+        borderStyle: 'solid',
+        borderWidth: '2px',
+        borderColor: '#39A094',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    button: {
+        height: 100,
+        width:300,
+        fontSize: 30
+    }
+}
 
 class Home extends Component {
     gotoFeelForm = () => {
         this.props.history.push('/FeelForm')
     }
+
     render() {
+        const { classes } = this.props;
         return (
-            <div>
-                <h1 style={{ backgroundColor: 'red' }}>Home Page</h1>
-                <button onClick={this.gotoFeelForm}>
-                    {'To feel form!'}
-                </button>
+            <div className={classes.form}>
+            <MuiThemeProvider theme={buttonTheme}>
+                <Button color="primary" className={classes.button}>
+                    Start Feedback
+                </Button>
+            </MuiThemeProvider>
             </div>
         );
     }
 }
 
-export default withRouter(Home);
+export default withRouter(withStyles(styles)(Home));
--- src/components/SupportForm/SupportForm.jsx
+++ src/components/SupportForm/SupportForm.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {withRouter} from 'react-router-dom';
+import { compose } from 'redux'
  
 class SupportForm extends Component {
     gotoCommentForm = () => {
