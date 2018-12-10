eb8117fc1326d02a727d6dd5fecad25d2712c510
Admin <admin@Admins-MBP.PK5001Z>
2018-12-09 20:26:07 -0600

    Adds review form along with redux support and allows user to send data do DB

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
+const pool = require('./modules/pool');
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
@@ -9,7 +10,20 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
+app.get('/feedback',(req,res)=>{
+    console.log(``);
+})
 
+app.post('/feedback',(req,res)=>{
+    console.log(`Trying to post`,req.body);
+    const queryString = 'INSERT INTO "feedback" ("feeling", "understanding", "support", "comments","flagged") VALUES ($1, $2, $3,$4,FALSE);';
+    pool.query(queryString,[req.body.feel, req.body.understand, req.body.support, req.body.comment]).then(result=>{
+        res.sendStatus(204);
+    }).catch(err=>{
+        console.log(`Error from DB in post: ${err}`);
+        res.sendStatus(500);
+    })
+})
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
---
+++ src/components/ReviewForm/ReviewForm.jsx
@@ -0,0 +1,159 @@
+// *----------*  *----------*
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Axios from 'axios';
+
+// *----------*  *----------*
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Select from '@material-ui/core/Select';
+import FilledInput from '@material-ui/core/FilledInput';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+
+
+
+
+const styles = {
+    form: {
+        width: '60%',
+        height: 700,
+        margin: '40px 10% 10px 10%',
+        backgroundColor: 'white',
+        borderRadius: '6px',
+        borderStyle: 'solid',
+        borderWidth: '2px',
+        borderColor: '#39A094',
+        display: 'inline-block',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    container: {
+        width: '100%',
+        display: 'inline-block'
+    }
+}
+
+// The entire store is passed in to this function
+const mapReduxToProps = (reduxStore) => ({
+    reducer: reduxStore.reviewReducer
+});
+
+class ReviewForm extends Component {
+
+    state = {
+        feelResponse: '',
+        understandResponse: '',
+        supportResponse: '',
+        commentResponse: ''
+    }
+
+    componentDidMount() {
+        this.setState({
+            feelResponse: this.props.reducer.feel,
+            understandResponse: this.props.reducer.understand,
+            supportResponse: this.props.reducer.support,
+            commentResponse: this.props.reducer.comment
+        })
+    }
+
+    handleChange = (event) => {
+        console.log(event.target);
+        this.props.dispatch({ type: 'ADD_INFO', payload: { key: event.target.name, value: event.target.value } })
+    }
+
+    handleSubmitClick = () => {
+        // send reducer to DB
+        Axios.post('/feedback',this.props.reducer).then(response=>{
+            // clear state and reducer
+            // go to home :)
+
+        }).catch(err=>{
+            console.log(`Error from server in post: ${err}`);
+        })
+    }
+    render() {
+
+        const { classes } = this.props;
+
+        return (
+            <div>
+                <div className={classes.form}>
+                    <h3 style={{ color: '#bbb' }}> - 5 / 5 - </h3>
+                    <h1 style={{ color: '#39A094', padding: '40px 0px' }}> Review </h1>
+                    <div>
+                        <form className={classes.container} noValidate autoComplete="off">
+                            <FormControl style={{width: '20%', margin: '0px ' }} variant="filled" className={classes.formControl}>
+                            <InputLabel>Feeling</InputLabel>
+                                <Select
+                                    value={this.props.reducer.feel}
+                                    onChange={this.handleChange}
+                                    input={<FilledInput name="feel" id="filled-age-simple" />}
+                                >
+                                    <MenuItem value="">
+                                        <em>None</em>
+                                    </MenuItem>
+                                    <MenuItem value={1}>1 - I'm feeling stressed </MenuItem>
+                                    <MenuItem value={2}>2</MenuItem>
+                                    <MenuItem value={3}>3</MenuItem>
+                                    <MenuItem value={4}>4</MenuItem>
+                                    <MenuItem value={5}>5 - I'm feeling stellar!</MenuItem>
+                                </Select>
+                            </FormControl>
+                            <FormControl style={{width: '20%', margin: '10px 40%' }} variant="filled" className={classes.formControl}>
+                            <InputLabel>Understanding</InputLabel>
+                                <Select
+                                    value={this.props.reducer.understand}
+                                    onChange={this.handleChange}
+                                    input={<FilledInput name="understand" id="filled-age-simple" />}
+                                >
+                                    <MenuItem value="">
+                                        <em>None</em>
+                                    </MenuItem>
+                                    <MenuItem value={1}>1 - I'm Completely lost </MenuItem>
+                                    <MenuItem value={2}>2</MenuItem>
+                                    <MenuItem value={3}>3</MenuItem>
+                                    <MenuItem value={4}>4</MenuItem>
+                                    <MenuItem value={5}>5 - I'm underwhelmed </MenuItem>
+                                </Select>
+                            </FormControl>
+                            <FormControl style={{width: '20%', margin: '10px 40%' }} variant="filled" className={classes.formControl}>
+                            <InputLabel>Support</InputLabel>
+                                <Select
+                                    value={this.props.reducer.support}
+                                    onChange={this.handleChange}
+                                    input={<FilledInput name="support" id="filled-age-simple" />}
+                                >
+                                    <MenuItem value="">
+                                        <em>None</em>
+                                    </MenuItem>
+                                    <MenuItem value={1}>1 - I feel abandoned </MenuItem>
+                                    <MenuItem value={2}>2</MenuItem>
+                                    <MenuItem value={3}>3</MenuItem>
+                                    <MenuItem value={4}>4</MenuItem>
+                                    <MenuItem value={5}>5 - I feel completely supported </MenuItem>
+                                </Select>
+                            </FormControl>
+                            <TextField
+                                name="comment"
+                                label="Comments"
+                                className={classes.textField}
+                                value={this.props.reducer.comment}
+                                onChange={this.handleChange}
+                                margin="normal"
+                            />
+                        </form>
+                    </div>
+                    <Button style={{ display: 'block', width: '20%', margin: '60px 40%' }} variant="contained" color="primary" className={classes.button} onClick={this.handleSubmitClick}>
+                        Submit
+                        </Button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default withRouter(withStyles(styles)(connect(mapReduxToProps)(ReviewForm)));
